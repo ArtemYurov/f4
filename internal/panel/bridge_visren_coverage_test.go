7c95d80d510032bc8b596b5d5abf72888b01fbef
unxed <ivan.sorokin.tech@gmail.com>
2026-09-09 21:30:50 +0200

    test: cover visren editor bridge

---
+++ internal/panel/bridge_visren_coverage_test.go
@@ -0,0 +1,47 @@
+package panel
+
+import (
+	"testing"
+
+	"github.com/unxed/f4/internal/editor"
+	"github.com/unxed/f4/internal/testutil"
+	"github.com/unxed/f4/plugins/visren"
+	"github.com/unxed/vtui"
+)
+
+func TestOpenVisRenEditorCreatesTemporaryEditorScreen(t *testing.T) {
+	t.Cleanup(testutil.SwapFrameManager(t))
+	scr := vtui.NewSilentScreenBuf()
+	scr.AllocBuf(100, 30)
+	vtui.FrameManager.Init(scr)
+
+	pf := &PanelsFrame{LastW: 100, LastH: 30}
+	err := pf.OpenVisRenEditor(visren.EditorRequest{
+		Title:      "VisRen rules",
+		Content:    []byte("*.go => go\n"),
+		CursorLine: 2,
+		CursorCol:  4,
+	})
+	if err != nil {
+		t.Fatalf("OpenVisRenEditor() error = %v", err)
+	}
+
+	if len(vtui.FrameManager.Screens) != 1 {
+		t.Fatalf("screens = %d, want 1", len(vtui.FrameManager.Screens))
+	}
+	screen := vtui.FrameManager.Screens[0]
+	if len(screen.Frames) != 1 {
+		t.Fatalf("frames = %d, want 1", len(screen.Frames))
+	}
+	ev, ok := screen.Frames[0].(*editor.EditorView)
+	if !ok {
+		t.Fatalf("screen frame has type %T, want *editor.EditorView", screen.Frames[0])
+	}
+	t.Cleanup(ev.Close)
+	if ev.DisplayTitle != "VisRen rules" {
+		t.Fatalf("editor title = %q, want %q", ev.DisplayTitle, "VisRen rules")
+	}
+	if ev.CursorLine != 2 || ev.CursorPos != 4 {
+		t.Fatalf("cursor = (%d, %d), want (2, 4)", ev.CursorLine, ev.CursorPos)
+	}
+}
